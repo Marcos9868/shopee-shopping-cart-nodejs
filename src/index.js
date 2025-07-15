3b19bda3113dd15c4f2b68721b051195b6e871f9
Marcos9868 <marcos_m_ferreira@outlook.com>
2025-07-15 19:00:52 -0300

    Feat: add simple flush of shopee shopping cart

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-const cart = [];
+import createItem from "./services/item.js";
+import * as cartService from "./services/cart.js"
+
+const myCart = [];
+const myWishList = [];
+
+const item1 = await createItem("Machenike Keyboard K500 B61", 129.99, 1);
+const item2 = await createItem("Machenike Keyboard K500 B84", 159.99, 3);
+
+await cartService.addItem(myCart, item1);
+await cartService.addItem(myWishList, item2);
+
+console.log("Shopee Cart Total Items");
+const total = await cartService.calculateTotalItems(myCart);
+const totalWishList = await cartService.calculateTotalItems(myWishList);
+console.log(total);
+console.log(totalWishList);
